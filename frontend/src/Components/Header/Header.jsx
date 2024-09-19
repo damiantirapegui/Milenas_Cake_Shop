348aee8eb679f0a67febc9a1b8d8a1dd898e8d78
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-09-19 14:21:24 +0200

    added hamburger menu with nav menu

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,14 +1,75 @@
+import { useState } from "react";
 import "./Header.css";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const toggleHamburgerMenu = () => {
+    if (isOpen) {
+      setIsClosing(true);
+      setTimeout(() => {
+        setIsOpen(false);
+        setIsClosing(false);
+      }, 300);
+    } else {
+      setIsOpen(true);
+      setIsClosing(false);
+    }
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <header className="header">
       <h1 className="title">Milenas tårtor</h1>
-      <div className="burger-menu">
+      <div
+        className={`burger-menu ${isOpen ? "open" : ""}`}
+        onClick={toggleHamburgerMenu}
+      >
         <div className="strech strech--1"></div>
         <div className="strech strech--2"></div>
         <div className="strech strech--3"></div>
       </div>
+
+      {(isOpen || isClosing) && ( // Visa menyn om den är öppen eller stängs
+        <nav
+          className={`nav-menu ${isOpen ? "open" : ""} ${
+            isClosing ? "closing" : ""
+          }`}
+        >
+          <ul>
+            <li onClick={toggleDropdown} className="dropdown-toggle">
+              Cakes
+            </li>
+            {isDropdownOpen && (
+              <ul className="dropdown">
+                <li>
+                  <a href="/wedding-cakes">Show Wedding Cakes</a>
+                </li>
+                <li>
+                  <a href="/kids-cake">Show Birthday Cakes</a>
+                </li>
+                <li>
+                  <a href="/regular-cake">Show Regular Cakes</a>
+                </li>
+                <li>
+                  <a href="/cup-cake">Show Cup Cakes</a>
+                </li>
+              </ul>
+            )}
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
